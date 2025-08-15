b46caf59ac61eca9f668753a2f208f1a9f484798
Ujjwal Sharma <ujjwalshrm2@gmail.com>
2025-08-15 21:52:45 +0530

    widget-screens

---
+++ apps/widget/modules/widget/atoms/widget-atoms.ts
@@ -0,0 +1,5 @@
+import { atom } from "jotai";
+
+import { WidgetScreen } from "../types";
+
+export const screenAtom = atom<WidgetScreen>("auth");
--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useAtomValue } from "jotai";
+
+import { screenAtom } from "../../atoms/widget-atoms";
 import { WidgetAuthScreen } from "../screens/widget-auth-screen";
 
 interface WidgetViewProps {
@@ -7,9 +10,22 @@ interface WidgetViewProps {
 }
 
 export function WidgetView({ organizationId }: WidgetViewProps) {
+  const screen = useAtomValue(screenAtom);
+
+  const screenComponents = {
+    error: <p>TODO:Error</p>,
+    loading: <p>TODO:loading</p>,
+    auth: <WidgetAuthScreen />,
+    voice: <p>TODO:voice</p>,
+    inbox: <p>TODO:inbox</p>,
+    selection: <p>TODO:selections</p>,
+    chat: <p>TODO:chat</p>,
+    contact: <p>TODO:contact</p>,
+  };
+
   return (
     <main className="h-screen w-full flex flex-col overflow-hidden rounded-xl border ">
-      <WidgetAuthScreen />
+      {screenComponents[screen]}
     </main>
   );
 }
